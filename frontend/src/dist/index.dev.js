fe45c867e85405db10bad05a6a14028fa3ae84f6
WellingtonVicen <wellvicen@gmail.com>
2020-12-09 16:23:51 -0300

    Criando Template

---
+++ frontend/src/dist/index.dev.js
@@ -0,0 +1,17 @@
+"use strict";
+
+var _react = _interopRequireDefault(require("react"));
+
+var _reactDom = _interopRequireDefault(require("react-dom"));
+
+require("./index.css");
+
+var _App = _interopRequireDefault(require("./main/App"));
+
+var _reportWebVitals = _interopRequireDefault(require("./reportWebVitals"));
+
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { "default": obj }; }
+
+_reactDom["default"].render();
+
+(0, _reportWebVitals["default"])();

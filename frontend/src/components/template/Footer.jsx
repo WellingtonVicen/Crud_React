717f75e497db0b42202b53010caad349873772cc
WellingtonVicen <wellvicen@gmail.com>
2020-12-10 14:06:46 -0300

    Componente Rodapé

--- frontend/src/components/template/Footer.jsx
+++ frontend/src/components/template/Footer.jsx
@@ -3,5 +3,8 @@ import React from 'react'
 
 export default props =>
 <footer className="footer">
-    Rodapé
+   <span>
+       Desenvolvido com <i className="fa fa-heart text-danger"></i> por
+       <strong> Wellington <span className="text-danger">Vicencio</span></strong>
+   </span>
 </footer>
